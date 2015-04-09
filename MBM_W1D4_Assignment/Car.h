67005a6252929d8dbf3c61cc7f8702f0003aee4d
Hemant Torsekar <hvt@me.com>
2015-04-09 16:32:33 -0400

    Initial commit with Car.h and Car.m setup. drive method specified in Car.h

---
+++ MBM_W1D4_Assignment/Car.h
@@ -0,0 +1,19 @@
+//
+//  Car.h
+//  MBM_W1D4_Assignment
+//
+//  Created by Hemant V. Torsekar on 2015-04-09.
+//  Copyright (c) 2015 Hemant V. Torsekar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject {
+
+}
+
+@property   (copy) NSString *model;
+
+    - (void)drive;
+
+@end
---
+++ MBM_W1D4_Assignment/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  MBM_W1D4_Assignment
+//
+//  Created by Hemant V. Torsekar on 2015-04-09.
+//  Copyright (c) 2015 Hemant V. Torsekar. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
