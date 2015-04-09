bba10a3e7390736e2a36ae4996aa1fa916cca031
Hemant Torsekar <hvt@me.com>
2015-04-09 17:00:37 -0400

    accessor/mutator for model

--- MBM_W1D4_Assignment/main.m
+++ MBM_W1D4_Assignment/main.m
@@ -7,11 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Car.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        Car *toyota = [[Car alloc] init];
+        
+        [toyota setModel:@"Toyota Corolla"];
+        NSLog(@"Changed the car to %@", toyota.model);
+        
+        [toyota drive];
     }
     return 0;
 }
