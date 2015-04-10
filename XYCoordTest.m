4b69ea139ab091ed9f892b8105e3b117fdac1acc
Hemant Torsekar <hvt@me.com>
2015-04-10 08:58:24 -0400

    Added file XYCoordTest.m to test the accessor and mutators for xCoord and yCoord

---
+++ XYCoordTest.m
@@ -0,0 +1,17 @@
+//
+//  XYCoordTest.m
+//  MBM_W1D4_Assignment
+//
+//  Created by Hemant V. Torsekar on 2015-04-10.
+//  Copyright (c) 2015 Hemant V. Torsekar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "XYPoint.h"
+
+int main2(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
