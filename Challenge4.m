3440dc84ced1a09f3f123197f3b6996511198ad6
Hemant Torsekar <hvt@me.com>
2015-04-09 23:19:57 -0400

    Advantage to having classes that have similar methods/actions, so you can let them inherit from a parent class.

---
+++ Challenge4.m
@@ -0,0 +1,19 @@
+//
+//  Challenge4.m
+//  MBM_W1D4_Assignment
+//
+//  Created by Hemant V. Torsekar on 2015-04-09.
+//  Copyright (c) 2015 Hemant V. Torsekar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//[myVehicle prep];
+//[myVehicle getGas];
+//[myVehicle service];
+
+
+// The advantage of two objects with similar methods/actions is that can inherit from one parent class.  This reduces repetitive class methods for two classes, that share the same methods. Makes the class easily managed by altering the parent, and reduces duplication in the codebase.
+
+
